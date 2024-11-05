f5089b66dec0ba687720cafdb439861785a1cfaa
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-11-05 16:18:48 +0100

    kosar

--- src/components/Kosar.js
+++ src/components/Kosar.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import { KosarContext } from '../contexts/KosarContext';
 
-function Kosar() {
-    const {kosaram} = useContext(KosarContext);
-  return (
-    
-  )
+function Kosar(props) {
+   /*  const {kosarL} = useContext(KosarContext);
+    return(
+    kosarL.map((termek)=>{
+        return <div>{termek.title}</div>
+    })
+    ) */
 }
 
 export default Kosar
